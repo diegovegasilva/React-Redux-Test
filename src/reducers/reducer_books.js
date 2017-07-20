4beb01e87444ac5cca2ffa1cd04699f822e9777d
Vega Silva <Diego.Vega@tecnocom.es>
2017-07-20 14:49:48 +0200

    video 39

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Javascript the good parts'},
+        {title: 'Harry Potter'},
+        {title: ' dark tower'},
+        {title: 'Lord of the Rings'}
+    ]
+}
